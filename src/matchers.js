8d101fcb0870dbec5b4da29950f807e932708814
joaopiopedreira <joaopiopedreira@gmail.com>
2018-02-06 12:19:34 +0000

    accept international numbers (USA, UK, Ireland, Portugal, France, Germany)

--- src/matchers.js
+++ src/matchers.js
@@ -36,7 +36,7 @@ export default [
   // Phone override
   {
     id: 'phone',
-    regex: /(?:(?:\(?(?:0(?:0|11)\)?[\s-]?\(?|\+)(1|44|33|351|353|49)\)?[\s-]?(?:\(?0\)?[\s-]?)?)|(?:\(?0))(?:(?:\d{5}\)?[\s-]?\d{4,5})|(?:\d{4}\)?[\s-]?\d{5,6})|(?:\d{4}\)?[\s-]?(?:\d{5}|\d{3}[\s-]?\d{3}))|(?:\d{3}\)?[\s-]?\d{3}[\s-]?\d{3,4})|(?:\d{2}\)?[\s-]?\d{4}[\s-]?\d{4}))(?:[\s-]?(?:x|ext\.?|\#)\d{3,4})?/g,
+    regex: /(?:(?:\(?(?:0(?:0|11)\)?[\s-]?\(?|\+)(1|44|33|351|353|49)\)?[\s-]?(?:\(?0\)?[\s-]?)?)|(?:\(?0))(?:(?:\d{5}\)?[\s-]?\d{4,5})|(?:\d{4}\)?[\s-]?\d{5,6})|(?:\d{4}\)?[\s-]?(?:\d{5}|\d{3}[\s-]?\d{3}))|(?:\d{3}\)?[\s-]?\d{3}[\s-]?\d{3,4})|(?:\d{2}\)?[\s-]?\d{4}[\s-]?\d{4}))(?:[\s-]?(?:x|ext\.?|#)\d{3,4})?/g,
     Match: Autolinker.Util.extend(Autolinker.match.Match, {
       constructor(cfg) {
         Autolinker.match.Match.prototype.constructor.call(this, cfg);
@@ -49,7 +49,7 @@ export default [
         return this.number;
       },
       getAnchorHref() {
-        return 'tel:' + this.number;
+        return `tel:${this.number}`;
       },
       getAnchorText() {
         return this.matchedText;
